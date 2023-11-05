2267d4130d2797187998d96494aeb0b2493b1ce8
Siddharth <siddharth.iyer123@gmail.com>
2023-11-05 16:30:48 -0600

    Backend functionality for adding new projects work with frontend

--- backend/routes/project_routes.py
+++ backend/routes/project_routes.py
@@ -14,11 +14,30 @@ CORS(project_bp)
 @project_bp.route("/create", methods=["POST"])
 def create_project_route():
     data = request.get_json()
-    project_name = data.get("name")
-    if not project_name:
-        return jsonify({"message": "Project name is required"}), 400
-    project = create_project(project_name)
-    return jsonify(project), 201
+    project_id = int(data.get("projectId"))
+    project_name = data.get("projectName")
+
+    if not data:
+        return jsonify({"message": "No data provided"}), 400
+
+    # Fetch the list of project IDs and names
+    project_ids = [item['project_id'] for item in supabase.table("Projects").select("project_id").execute()['data']]
+    project_names = [item['project_name'] for item in supabase.table("Projects").select("project_name").execute()['data']]
+
+    print(project_ids)
+    print(project_names)
+
+    # Project ID already in use
+    if project_id in project_ids:
+        return jsonify({"Error Message": "ERROR WITH ID"}), 403
+
+    # Project name already in use
+    if project_name in project_names:
+        return jsonify({"Error Message": "ERROR WITH NAME"}), 403
+
+    else:
+        project = create_project(project_name, project_id)
+        return jsonify(project), 201
 
 @project_bp.route("/getprojects", methods=["GET"])
 def get_projects_route():
--- backend/services/project_service.py
+++ backend/services/project_service.py
@@ -6,21 +6,22 @@ supabaseUrl = "https://tpbjxnsgkuyljnxiqfsz.supabase.co"
 supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwYmp4bnNna3V5bGpueGlxZnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxMTI5NjIsImV4cCI6MjAxMTY4ODk2Mn0.lTYIrdhUz9qe2PdwKN-zbBDScqIHA7u97iatoIazqmc"
 supabase: Client = create_client(supabaseUrl, supabaseKey)
 
-def create_project(name):
-    data = {"project_name": name}
+def create_project(name, id):
+    data = {"project_name": name,
+            "project_id" : id}
     project_response, error = supabase.table("Projects").insert(data).execute()
-    
+
     print("Supabase Response:", project_response)
     print("Supabase Error:", error)
 
     if error:
         logging.error(f"Error creating project: {error}")
         return None
-    
+
     if 'data' not in project_response or not project_response['data']:
         logging.error(f"Unexpected response from Supabase: {project_response}")
         return None
-    
+
     return project_response['data'][0]
 
 
@@ -29,12 +30,12 @@ def get_projects():
 
     # Extract the project names into a list of strings
     project_names = [project['project_name'] for project in data.data]
-    
+
     if error:
         logging.error(f"Error fetching projects: {error}")
         return []
-    
+
     print(data)
     print(project_names)
-    
+
     return project_names
